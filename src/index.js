77b35ab367f1ea06338e0886bc6ac4895228a267
yanahiro777 <58253496+yanahiro777@users.noreply.github.com>
2022-12-05 09:26:52 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// import "./styles.css";
+
+// document.getElementById("app").innerHTML = `
+// <h1>Hello Vanilla!</h1>
+// <div>
+//   We use the same configuration as Parcel to bundle this sandbox, you can find more
+//   info about Parcel
+//   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+// </div>
+// `;
+
+// const fun3= (num1, num2) => {
+//   return num1 + num2;
+// }
+// console.log(fun3(10,20)):
